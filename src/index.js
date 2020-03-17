49cbe26e6ab33a1937cb4da0874eb224610a509b
ShaDoKaiKan <sha.alia95@gmail.com>
2020-03-17 13:14:24 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let currentLength;
+    
+    function remove() {
+      currentLength = str.length;
+      bracketsConfig.forEach(item => str = str.replace(item.join(''), ''));
+      if (currentLength !== str.length) {
+        remove();
+      } 
+    };
+    remove();
+    
+    if (str.length === 0) {
+        return true;
+    } else return false;
+    
+  
 }
+
